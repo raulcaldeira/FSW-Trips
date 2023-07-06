0c0f17e7f1c1e5e0e8660a02ed7af6b8582ce212
Raul Santana <ti.raul77@gmail.com>
2023-07-05 22:49:51 -0300

    feat: add location description

---
+++ prisma/migrations/20230706012837_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescriptions" TEXT NOT NULL DEFAULT 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don''t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn''t anything embarrassing hidden in the middle of text.';
