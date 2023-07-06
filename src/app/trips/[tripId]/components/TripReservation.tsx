50e2c1811b2e949ff40bbab32f89b339f278f532
Raul Santana <ti.raul77@gmail.com>
2023-07-05 21:07:10 -0300

    style: add border to trip reservation component

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -13,23 +13,23 @@ interface TripReservationProps {
 
 const TripReservation = ({trip}: TripReservationProps) => {
     return(
-        <div>
-            <div className="flex flex-col px-5">
-                <div className="flex gap-4">
-                    <DatePicker placeholderText="Data de Início" onChange={() => {}} className="w-full"/>
-                    <DatePicker placeholderText="Data Final" onChange={() => {}} className="w-full" />
-                </div>
-
-                <Input placeholder={`Número de hóspedes (máx: ${trip.maxGuests.toString()})`} className="mt-4" />
-
-                <div className="flex justify-between mt-3">
-                    <p className="font-medium text-sm text-primaryDarker">Total: </p>
-                    <p className="font-medium text-sm text-primaryDarker">R$2500 </p>
-                </div>
-
-                <Button className="mt-3"> Reservar agora </Button>
-
+        <div className="flex flex-col px-5">
+            <div className="flex gap-4">
+                <DatePicker placeholderText="Data de Início" onChange={() => {}} className="w-full"/>
+                <DatePicker placeholderText="Data Final" onChange={() => {}} className="w-full" />
             </div>
+
+            <Input placeholder={`Número de hóspedes (máx: ${trip.maxGuests.toString()})`} className="mt-4" />
+
+            <div className="flex justify-between mt-3">
+                <p className="font-medium text-sm text-primaryDarker">Total: </p>
+                <p className="font-medium text-sm text-primaryDarker">R$2500 </p>
+            </div>
+            
+            <div className="pb-10 border-b border-grayLighter w-full">
+                <Button className="mt-3 w-full"> Reservar agora </Button>
+            </div>
+
         </div>
     )
 };
