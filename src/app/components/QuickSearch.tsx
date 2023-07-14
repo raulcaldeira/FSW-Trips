991704ad835201ce4f787ecdf267d2cb4a13571b
Raul Santana <ti.raul77@gmail.com>
2023-07-14 01:24:45 -0300

    feat: add quick search

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const QuickSearch = () => {
     return(
@@ -10,25 +11,27 @@ const QuickSearch = () => {
             </div>
 
             <div className="flex w-full justify-around mt-5">
-                <div className="flex flex-col items-center gap-1">
+
+                <Link href={"/trips/search?text=hotel"} className="flex flex-col items-center gap-1">
                     <Image width={35} height={35} src="/hotel-icon.png" alt="Hotel" />
                     <p className="text-sm text-grayPrimary">Hotel</p>
-                </div>
+                </Link>
 
-                <div className="flex flex-col items-center gap-1">
+                <Link href={"/trips/search?text=fazenda"} className="flex flex-col items-center gap-1">
                     <Image width={35} height={35} src="/farm-icon.png" alt="Fazenda" />
                     <p className="text-sm text-grayPrimary">Fazenda</p>
-                </div>
+                </Link>
 
-                <div className="flex flex-col items-center gap-1">
+                <Link href={"/trips/search?text=chalé"} className="flex flex-col items-center gap-1">
                     <Image width={35} height={35} src="/cottage-icon.png" alt="Chalé" />
                     <p className="text-sm text-grayPrimary">Chalé</p>
-                </div>
+                </Link>
 
-                <div className="flex flex-col items-center gap-1">
+                <Link href={"/trips/search?text=pousada"} className="flex flex-col items-center gap-1">            
                     <Image width={35} height={35} src="/inn-icon.png" alt="Hotel" />
                     <p className="text-sm text-grayPrimary">Pousada</p>
-                </div>
+                </Link>
+
             </div>
         </div>
     )
