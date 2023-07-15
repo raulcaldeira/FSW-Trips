5a79e5939b406d259740f15cbd172981bc384e12
Raul Santana <ti.raul77@gmail.com>
2023-07-15 17:18:10 -0300

    style: adapt my trips page to desktop

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,69 +1,61 @@
-'use client'
+"use client";
 
-import { Prisma } from "@prisma/client";
+import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import UserReservationItem from "./components/UserReservationItem";
+import { Prisma } from "@prisma/client";
 import Link from "next/link";
+
+import UserReservationItem from "./components/UserReservationItem";
 import Button from "@/components/Button";
 
-
-
-
 const MyTrips = () => {
+  const [reservations, setReservations] = useState<
+    Prisma.TripReservationGetPayload<{
+      include: { trip: true };
+    }>[]
+  >([]);
 
-    const [reservations, setReservations] = useState<Prisma.TripReservationGetPayload<{include: {trip: true}}>[]>([])
+  const { status, data } = useSession();
 
-    const { status, data } = useSession()
+  const router = useRouter();
 
-    const router = useRouter()
+  const fetchReservations = async () => {
+    const response = await fetch(`/api/user/${(data?.user as any)?.id}/reservations`);
 
-    const fetchReservations = async () => {
-        const response = await fetch(`/api/user/${(data?.user as any).id}/reservations`)
-        const json = await response.json();
+    const json = await response.json();
 
-        setReservations(json)
+    setReservations(json);
+  };
 
-        console.log(json)
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      return router.push("/");
     }
 
-    useEffect(()=>{
-        if(status == "unauthenticated"){
-           return router.push('/')
-        }
+    fetchReservations();
+  }, [status]);
 
-        const fetchReservations = async () => {
-            const response = await fetch(`/api/user/${(data?.user as any)?.id}/reservations`)
-            const json = await response.json();
-
-            setReservations(json)
-
-            console.log(json)
-        }
-
-        fetchReservations()
-
-    }, [status])
-
-    return(
-        <div className="container mx-auto p-5">
-            <h1 className="font-semibold text-primaryDarker text-xl">
-                Minhas viagens
-            </h1>
-            {reservations.length > 0 ? (
-                reservations?.map(reservation => <UserReservationItem key={reservation.id} reservation={reservation} fetchReservations={fetchReservations}/>)
-            ): (
-                <div className="flex flex-col">
-                    <p className="mt-2 font-medium text-primaryDarker"> Você ainda não tem nenhuma reserva! =( </p>
-
-                    <Link href="/">
-                        <Button className="w-full mt-2">Fazer reserva</Button>
-                    </Link>
-                </div>
-            )}
+  return (
+    <div className="container mx-auto p-5 lg:pb-10">
+      <h1 className="font-semibold text-primaryDarker text-xl lg:mb-5">Minhas Viagens</h1>
+      {reservations.length > 0 ? (
+        <div className="flex flex-col lg:grid lg:grid-cols-3 lg:gap-14">
+          {reservations?.map((reservation) => (
+            <UserReservationItem fetchReservations={fetchReservations} key={reservation.id} reservation={reservation} />
+          ))}
         </div>
-    )
-}
+      ) : (
+        <div className="flex flex-col lg:max-w-[500px]">
+          <p className="mt-2 font-medium text-primaryDarker">Você ainda não tem nenhuma reserva! =(</p>
+
+          <Link href="/">
+            <Button className="w-full mt-2 lg:mt-5">Fazer reserva</Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default MyTrips;
